3ee4152276bf604d3ac3d0eb1500415cfa0a0533
Syahrul Ramadhan <0320200028@polman.astra.ac.id>
2021-05-25 14:50:24 +0700

    nambah label di form1

--- LatihanReport/Form1.Designer.cs
+++ LatihanReport/Form1.Designer.cs
@@ -30,19 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.myPracticeDataSet = new LatihanReport.MyPracticeDataSet();
-            this.tBMPRODUCTBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.tB_M_PRODUCTTableAdapter = new LatihanReport.MyPracticeDataSetTableAdapters.TB_M_PRODUCTTableAdapter();
             this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.itemnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.designDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.expiredDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tBMPRODUCTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.myPracticeDataSet = new LatihanReport.MyPracticeDataSet();
+            this.tB_M_PRODUCTTableAdapter = new LatihanReport.MyPracticeDataSetTableAdapters.TB_M_PRODUCTTableAdapter();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.myPracticeDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tBMPRODUCTBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myPracticeDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -57,24 +58,11 @@
             this.colorDataGridViewTextBoxColumn,
             this.expiredDateDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.tBMPRODUCTBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 81);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 125);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(544, 180);
+            this.dataGridView1.Size = new System.Drawing.Size(816, 277);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // myPracticeDataSet
-            // 
-            this.myPracticeDataSet.DataSetName = "MyPracticeDataSet";
-            this.myPracticeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tBMPRODUCTBindingSource
-            // 
-            this.tBMPRODUCTBindingSource.DataMember = "TB_M_PRODUCT";
-            this.tBMPRODUCTBindingSource.DataSource = this.myPracticeDataSet;
-            // 
-            // tB_M_PRODUCTTableAdapter
-            // 
-            this.tB_M_PRODUCTTableAdapter.ClearBeforeFill = true;
             // 
             // iDDataGridViewTextBoxColumn
             // 
@@ -106,11 +94,26 @@
             this.expiredDateDataGridViewTextBoxColumn.HeaderText = "expiredDate";
             this.expiredDateDataGridViewTextBoxColumn.Name = "expiredDateDataGridViewTextBoxColumn";
             // 
+            // tBMPRODUCTBindingSource
+            // 
+            this.tBMPRODUCTBindingSource.DataMember = "TB_M_PRODUCT";
+            this.tBMPRODUCTBindingSource.DataSource = this.myPracticeDataSet;
+            // 
+            // myPracticeDataSet
+            // 
+            this.myPracticeDataSet.DataSetName = "MyPracticeDataSet";
+            this.myPracticeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tB_M_PRODUCTTableAdapter
+            // 
+            this.tB_M_PRODUCTTableAdapter.ClearBeforeFill = true;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(181, 37);
+            this.button1.Location = new System.Drawing.Point(272, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.Size = new System.Drawing.Size(136, 35);
             this.button1.TabIndex = 1;
             this.button1.Text = "Export to PDF";
             this.button1.UseVisualStyleBackColor = true;
@@ -118,29 +121,42 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(300, 37);
+            this.button2.Location = new System.Drawing.Point(450, 57);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.Size = new System.Drawing.Size(135, 35);
             this.button2.TabIndex = 2;
             this.button2.Text = "Export to Excel";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "syahrul terakhir edit";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 262);
+            this.ClientSize = new System.Drawing.Size(855, 403);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Export into PDF and Excel";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.myPracticeDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tBMPRODUCTBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myPracticeDataSet)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,6 +173,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn expiredDateDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
